241f5b7bae75096841bf39eacc235b3a4ac7e1eb
Arno <arn.hovas@gmail.com>
2026-02-20 22:07:15 +0400

    Add poor implementation hover/draw random color effect

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 const container = document.getElementById('container');
-const SQUARES = 16;
-const SQUARES_SIZE = 100 / SQUARES;
+const squareSizeBtn = document.getElementById('square-size');
 
-for (let i = 0; i < SQUARES; i++) {
-  for (let j = 0; j < SQUARES; j++) {
-    container.append(createSquare());
+function handleSquareSize() {
+  const size = +prompt('Enter square size', 16);
+
+  if (!size || size >= 100 || size === null) return;
+
+  const SQUARES = size;
+  const SQUARES_WIDTH = 100 / SQUARES;
+  renderSquares(SQUARES, SQUARES_WIDTH);
+}
+
+squareSizeBtn.addEventListener('click', handleSquareSize);
+
+function renderSquares(squares, width) {
+  container.innerText = '';
+
+  for (let i = 0; i < squares; i++) {
+    for (let j = 0; j < squares; j++) {
+      container.append(createSquare(width));
+    }
   }
 }
 
-function createSquare() {
+function createSquare(width) {
   const square = document.createElement('div');
-  square.style.width = `${SQUARES_SIZE}%`;
+  square.style.width = `${width}%`;
   square.classList.add('square');
 
   return square;
+}
+
+container.addEventListener('mouseover', (e) => {
+  const target = e.target;
+
+  e.stopPropagation();
+
+  target.addEventListener('mouseenter', (e) => {
+    e.currentTarget.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
+    console.log('click');
+  });
+});
+
+function randomColor() {
+  return Math.floor(Math.random() * 255);
 }
